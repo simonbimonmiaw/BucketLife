5bd25375020a3558610005f2a2b0d44796908530
ninadupont <ninadupont@users.noreply.github.com>
2018-02-13 20:17:18 +0100

    changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,2 @@
-var className = document.getElementsByClassName('inspiration')[0].className;
-  if(className === "inspiration inspiration--showing") {
-    document.getElementsByClassName('inspiration')[0].className = "inspiration";
-  } else {
-    document.getElementsByClassName('inspiration')[0].className = "inspiration inspiration--showing";
-  }
-}
-;
-
-/*var button = document.getElementById('slidebtn').onclick = function () {
-  var className = document.getElementsByClassName('content-wrapper')[0].className;
-  if(className === "content-wrapper content-wrapper--aside") {
-    document.getElementsByClassName('content-wrapper')[0].className = "content-wrapper";
-  } else {
-    document.getElementsByClassName('content-wrapper')[0].className = "content-wrapper content-wrapper--aside";
-  }
-
-}
-*/
+// var h3 = document.getElementsByTagName('h3')[0];
+//     h3.innerHTML = '<span>' + h3.innerHTML.split('').join('</span><span>') + '</span>';
